0073593745f94f27638269ccc498a40f04c29207
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-03-18 08:47:03 -0300

    Update desafio 1.py

--- desafio 1.py
+++ desafio 1.py
@@ -1,13 +1,10 @@
 import random
 
-# Sorteia dois números aleatórios entre 1 e 100
 num1 = random.randint(1, 100)
 num2 = random.randint(1, 100)
 
-# Soma os dois números
 soma = num1 + num2
 
-# Exibe os números sorteados e a soma
 print(f"Número 1: {num1}")
 print(f"Número 2: {num2}")
 print(f"Soma: {soma}")
