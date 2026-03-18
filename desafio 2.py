b6c191f00dd9ea7ed95e187db3fc834f332aa1d2
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-03-18 08:52:32 -0300

    Update desafio 2.py

--- desafio 2.py
+++ desafio 2.py
@@ -1,1 +1,11 @@
+def eh_par(numero):
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
 
+num = int(input("Digite um número: "))
+if eh_par(num):
+    print(f"O número {num} é par.")
+else:
+    print(f"O número {num} é ímpar.")
