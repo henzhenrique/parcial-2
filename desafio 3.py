e81fe1033b9410fa853dfc682e02337cf39f7f2f
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-03-25 08:45:44 -0300

    Update desafio 3.py

--- desafio 3.py
+++ desafio 3.py
@@ -1,1 +1,16 @@
+nome = []
 
+nome1 = input("digite o primeiro nome:")
+nome2 = input("digite o segundo nome:")
+nome3 = input("digite o terceiro nome:")
+nome4 = input("digite o quarto nome:")
+nome5 = input("digite o quinto nome:")
+
+nome.append(nome1)
+nome.append(nome2)
+nome.append(nome3)
+nome.append(nome4)
+nome.append(nome5)
+print("\nOs nomes digitados foram:")
+for n in nome:
+    print(n)
