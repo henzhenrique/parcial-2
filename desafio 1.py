e2fed8b2a2300e94b22de9ef9de11918288a3270
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-03-25 08:17:38 -0300

    Update desafio 1.py

--- desafio 1.py
+++ desafio 1.py
@@ -1,10 +1,10 @@
-numero1 = input("Digite o primeiro número: ")
 
-numero2 = input("Digite o segundo número: ")
+numero1 = input("Digite o primeiro numero")
+
+numero2 = input("Digite o segundo numero")
 
 numero1 = float(numero1)
 numero2 = float(numero2)
 
-soma = numero1 + numero2
-
-print("A soma dos números é:", soma)
+soma = numero1+numero2
+print(" a soma dos numeros é:", soma)
