86d0744b961c4c4a4d256125d7b27dcd8575aae7
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-03-31 09:40:27 -0300

    Update desafio 5.py

--- desafio 5.py
+++ desafio 5.py
@@ -1,1 +1,7 @@
+altura = float(input("digite a altura do triangulo:"))
+base = float(input("digite a base desse triangulo:"))
+
+area = (altura * base ) / 2
+
+print(f"a area do triangulo é: {area}" )
 
