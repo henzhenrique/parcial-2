6bec6d85663de64ce2b6cacd088ac45707bb214b
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-03-31 09:25:02 -0300

    Update desafio 4.py

--- desafio 4.py
+++ desafio 4.py
@@ -1,1 +1,19 @@
+num1 = float(input("Digite o primeiro número: "))
+op = input("Escolha a operação (+, -, *, /): ")
+num2 = float(input("Digite o segundo número: "))
 
+if op == "+":
+    resultado = num1 + num2
+elif op == "-":
+    resultado = num1 - num2
+elif op == "*":
+    resultado = num1 * num2
+elif op == "/":
+    if num2 != 0:
+        resultado = num1 / num2
+    else:
+        resultado = "erro, não é possivel dividir por 0"
+else:
+    resultado = "operação invalida"
+
+print("Resultado:", resultado)
