2e00a8a48c4ddd7cfb745085c5df7dfd29c89b2d
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-04-07 09:35:46 -0300

    Update desafio 1.py

--- desafio 1.py
+++ desafio 1.py
@@ -1,10 +1,10 @@
+# ter as 2 grandezas
+numero1 = input("Digite o primeiro numero:")
 
-numero1 = input("Digite o primeiro numero")
-
-numero2 = input("Digite o segundo numero")
+numero2 = input("Digite o segundo numero:")
 
 numero1 = float(numero1)
 numero2 = float(numero2)
-
+# somar os 2 numeros
 soma = numero1+numero2
 print(" a soma dos numeros é:", soma)
