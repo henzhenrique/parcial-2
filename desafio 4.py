b7876e31723e7beea5fa8f7f32e52858c2993084
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-04-08 19:50:51 -0300

    Improve input handling and add comments in desafio 4.py
    Refactor input prompts and add comments for clarity.

--- desafio 4.py
+++ desafio 4.py
@@ -1,7 +1,7 @@
-num1 = float(input("Digite o primeiro número: "))
+num1 = float(input("Digite o primeiro número: "))#escolher os numeros
 op = input("Escolha a operação (+, -, *, /): ")
 num2 = float(input("Digite o segundo número: "))
-
+#definir as operaçoes da calculadora
 if op == "+":
     resultado = num1 + num2
 elif op == "-":
@@ -15,5 +15,5 @@ elif op == "/":
         resultado = "erro, não é possivel dividir por 0"
 else:
     resultado = "operação invalida"
-
+# mostrar o resultado no final
 print("Resultado:", resultado)
