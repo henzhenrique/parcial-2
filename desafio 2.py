3b0f72ebe648cd882a04dc2c689d166d9603b4b6
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-04-08 19:49:18 -0300

    Update desafio 2.py

--- desafio 2.py
+++ desafio 2.py
@@ -1,9 +1,9 @@
 def eh_par(numero):
-    if numero % 2 == 0:
+    if numero % 2 == 0:#pra definir se o numero é par
         return True
     else:
         return False
-
+#pede pra pessoa digitar o numero.
 num = int(input("Digite um número: "))
 if eh_par(num):
     print(f"O número {num} é par.")
