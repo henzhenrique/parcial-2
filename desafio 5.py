8408d3e2fee0fe85c62fc3e0ce1b9c698b032e8a
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-04-08 19:52:01 -0300

    Add comments for input and area calculation
    Added comments to clarify input and area calculation.

--- desafio 5.py
+++ desafio 5.py
@@ -1,7 +1,7 @@
-altura = float(input("digite a altura do triangulo:"))
+altura = float(input("digite a altura do triangulo:"))# digitar altura e base do triangulo
 base = float(input("digite a base desse triangulo:"))
 
-area = (altura * base ) / 2
+area = (altura * base ) / 2# a formula da area do triangulo
 
 print(f"a area do triangulo é: {area}" )
 
