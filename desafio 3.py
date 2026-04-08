ae1c588ca8536c7efd9612732b0fbb3cded180d5
henzhenrique <henz.henrique@escola.pr.gov.br>
2026-04-08 17:29:02 -0300

    Update desafio 3.py

--- desafio 3.py
+++ desafio 3.py
@@ -1,11 +1,11 @@
 nome = []
-
+#ter os 5 nomes
 nome1 = input("digite o primeiro nome:")
 nome2 = input("digite o segundo nome:")
 nome3 = input("digite o terceiro nome:")
 nome4 = input("digite o quarto nome:")
 nome5 = input("digite o quinto nome:")
-
+#comando usado para listar os nomes
 nome.append(nome1)
 nome.append(nome2)
 nome.append(nome3)
